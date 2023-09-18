c86e6c04581b522b3d860cfe2b733e3be284b6ec
monstergog <aminhn95@gmail.com>
2023-09-18 08:30:57 -0700

    added studybuddy heroku link

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,7 +9,7 @@ function Portfolio() {
         <figure class="portfolio">
             <section id="project1" class="big-projects">
             <h4><a className="ProjectGitHub" href="https://github.com/Marinah1031/StudyBuddy">StudyBuddy</a></h4>
-                <a href="https://github.com/Marinah1031/StudyBuddy"><img src={StudyBuddy} alt="Project1" /></a>
+                <a href="https://study-buddy-p3-0d14fa2a37d6.herokuapp.com/"><img src={StudyBuddy} alt="Project1" /></a>
             </section>
             <section id="project2" class="small-projects">
             <h5><a className="ProjectGitHub" href="https://github.com/monstergog/Park-Talk">Park Talk</a></h5>
